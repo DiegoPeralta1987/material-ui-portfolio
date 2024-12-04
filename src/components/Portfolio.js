8617bcb363125b98de5e7bb808e3256712264b43
DIEGO <diegonly@gmail.com>
2024-12-04 16:01:43 -0300

    Ajustes varios

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,9 +10,9 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
-import project1 from "../images/html-css-javascript-lg.jpg";
-import project2 from "../images/html-css-javascript.jpg";
-import project3 from "../images/javascript-fullstack.jpg";
+import project1 from "../images/flota.png";
+import project2 from "../images/app.jpg";
+import project3 from "../images/factracion.png";
 import project4 from "../images/mern-stack.jpg";
 import project5 from "../images/react-redux.jpg";
 import project6 from "../images/react.png";
@@ -30,52 +30,50 @@ const useStyles = makeStyles((theme) => ({
 
 const projects = [
   {
-    name: "CONTROL DE FLOTA",
-    description: `Este sistema esta pensando para tener el control diario de los ingresos e egresos que genera
-    los moviles de la empresa, viajes, combustible, viaticos, etc`,
+    name: "Control de Flota",
+    description: `Este sistema está diseñado para gestionar de manera integral los ingresos y egresos diarios generados por la operación de los vehículos de la empresa. Permite un control detallado de viajes, consumo de combustible, viáticos, pagos de personal detallados y otros gastos asociados, optimizando la administración de recursos y mejorando la eficiencia operativa.`,
     image: project1,
     url: "https://prueba.tiquidevs.click/src/vistas/login.html"
   },
   {
-    name: "Project 2",
-    description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
-    consequatur magni quod nesciunt necessitatibus molestiae non\
-    eligendi, magnam est aliquam recusandae? Magnam soluta minus\
-    iste alias sunt veritatis nisi dolores!`,
+    name: "Red Social Guarderias",
+    description: `Un sistema diseñado para conectar a las familias con la guardería, funcionando como una red social institucional. Permite la visualización en tiempo real de actividades diarias, acceso a informes detallados sobre el desarrollo y progreso de los niños, y una galería de fotos de las actividades realizadas, fomentando la transparencia y fortaleciendo el vínculo entre la institución y los padres.`,
     image: project2,
+    url: "https://mis.tiquidevs.click"
   },
   {
-    name: "Project 3",
-    description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
-    consequatur magni quod nesciunt necessitatibus molestiae non\
-    eligendi, magnam est aliquam recusandae? Magnam soluta minus\
-    iste alias sunt veritatis nisi dolores!`,
+    name: "Sistema de Facturacion",
+    description: `Este sistema está diseñado para optimizar el control de inventarios y la gestión de facturación en tiempo real. Proporciona herramientas para: 
+    Gestión de Inventarios: Monitoreo detallado de existencias, alertas de stock bajo, y actualización automática tras ventas o ingresos de mercancías.
+    Facturación Eficiente: Emisión de comprobantes personalizados, historial de ventas, y cálculos automáticos de impuestos. 
+    Con una interfaz intuitiva, el sistema asegura precisión en las operaciones `,
     image: project3,
+    url: "https://inventario.tiquidevs.click/vistas/login.html"
   },
-  {
-    name: "Project 4",
-    description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
-    consequatur magni quod nesciunt necessitatibus molestiae non\
-    eligendi, magnam est aliquam recusandae? Magnam soluta minus\
-    iste alias sunt veritatis nisi dolores!`,
-    image: project4,
-  },
-  {
-    name: "Project 5",
-    description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
-    consequatur magni quod nesciunt necessitatibus molestiae non\
-    eligendi, magnam est aliquam recusandae? Magnam soluta minus\
-    iste alias sunt veritatis nisi dolores!`,
-    image: project5,
-  },
-  {
-    name: "Project 6",
-    description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
-    consequatur magni quod nesciunt necessitatibus molestiae non\
-    eligendi, magnam est aliquam recusandae? Magnam soluta minus\
-    iste alias sunt veritatis nisi dolores!`,
-    image: project6,
-  },
+  // {
+  //   name: "Project 4",
+  //   description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
+  //   consequatur magni quod nesciunt necessitatibus molestiae non\
+  //   eligendi, magnam est aliquam recusandae? Magnam soluta minus\
+  //   iste alias sunt veritatis nisi dolores!`,
+  //   image: project4,
+  // },
+  // {
+  //   name: "Project 5",
+  //   description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
+  //   consequatur magni quod nesciunt necessitatibus molestiae non\
+  //   eligendi, magnam est aliquam recusandae? Magnam soluta minus\
+  //   iste alias sunt veritatis nisi dolores!`,
+  //   image: project5,
+  // },
+  // {
+  //   name: "Project 6",
+  //   description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
+  //   consequatur magni quod nesciunt necessitatibus molestiae non\
+  //   eligendi, magnam est aliquam recusandae? Magnam soluta minus\
+  //   iste alias sunt veritatis nisi dolores!`,
+  //   image: project6,
+  // },
 ];
 
 const Portfolio = () => {
